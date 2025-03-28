98ef2b7928a28eb066893db43bd54f83d3e402c6
Timofei Beresnjev <bertimblch@gmail.com>
2025-03-28 23:09:40 +0200

    enable-locations-by-day

--- Assets/Global/Scripts/CityManager.cs
+++ Assets/Global/Scripts/CityManager.cs
@@ -16,6 +16,7 @@ public class CityManager : MonoBehaviour
     private bool isGameOver = false;
     private City city;
     private Dictionary<int, List<Event>> eventsByDay;
+    private Dictionary<int, LocationType> enablingLocationsByDay;
     private int previousDayEnergyDifference = 0;
 
     private void Awake()
@@ -23,40 +24,20 @@ public class CityManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);  // Ensures the CityManager persists across scenes
+            DontDestroyOnLoad(gameObject); // Ensures the CityManager persists across scenes
         }
         else
         {
-            Destroy(gameObject);  // Destroy duplicate instances of the CityManager
+            Destroy(gameObject); // Destroy duplicate instances of the CityManager
             return;
         }
-        
+
         city = new City();
         city.population = cityPopulation;
         city.maxDaysAmount = maxCityDaysAmount;
         city.locations = new List<Location>(FindObjectsByType<Location>(FindObjectsSortMode.None));
-        
-        eventsByDay = new Dictionary<int, List<Event>>();
-        // Initialize some events
-        Event chillDay = new Event(EventType.NO_IMPACT, "Chill day, no events.");
-        
-        Event earthquake = new Event(EventType.SOME_LOCATIONS, "Earthquake hits the city!");
-        earthquake.locationsEnergyModifier = new Dictionary<LocationType, int>
-        {
-            { LocationType.Factory, -50 },
-            { LocationType.Hospital, -30 }
-        };
-
-        Event protest = new Event(EventType.SOME_LOCATIONS, "Protest at the School.");
-        protest.locationsEnergyModifier = new Dictionary<LocationType, int>
-        {
-            { LocationType.School, 20 }
-        };
-
-        // Adding events to specific days
-        eventsByDay[1] = new List<Event> { chillDay };
-        eventsByDay[2] = new List<Event> { earthquake };
-        eventsByDay[3] = new List<Event> { protest };
+        InitializeEvents();
+        InitializeEnablingLocationsByDay();
     }
 
     private void Update()
@@ -96,7 +77,7 @@ public class CityManager : MonoBehaviour
     {
         int actualEnergy = StationManager.Instance.energyAmount;
         int neededEnergy = 0;
-        foreach (var location in city.locations)
+        foreach (var location in city.getEnabledLocations())
         {
             neededEnergy += location.currentDayEnergyAmount;
         }
@@ -120,6 +101,7 @@ public class CityManager : MonoBehaviour
     {
         city.SwitchNextDay();
         UpdateDayCounterUI();
+        EnableCurrentDayLocation();
         
         if (eventsByDay.ContainsKey(city.currentDay))
         {
@@ -148,7 +130,7 @@ public class CityManager : MonoBehaviour
     
     private void LogAllLocations()
     {
-        foreach (var location in city.locations)
+        foreach (var location in city.getEnabledLocations())
         {
             Debug.Log($"Location: {location.type}, Base Energy: {location.baseEnergyAmount}, Current Day Energy: {location.currentDayEnergyAmount}");
         }
@@ -168,5 +150,53 @@ public class CityManager : MonoBehaviour
         {
             finalMessage.text = message;  // Update text here
         }
+    }
+
+    private void EnableCurrentDayLocation()
+    {
+        LocationType locationTypeToEnable = enablingLocationsByDay[city.currentDay];
+        foreach (var location in city.locations)
+        {
+            if (location.type == locationTypeToEnable)
+            {
+                location.EnableLocation();
+                Debug.Log($"Location {locationTypeToEnable} enabled for day {city.currentDay}");
+            }
+        }
+    }
+    
+    private void InitializeEvents()
+    {
+        eventsByDay = new Dictionary<int, List<Event>>();
+        // Initialize some events
+        Event chillDay = new Event(EventType.NO_IMPACT, "Chill day, no events.");
+        
+        Event earthquake = new Event(EventType.SOME_LOCATIONS, "Earthquake hits the city!");
+        earthquake.locationsEnergyModifier = new Dictionary<LocationType, int>
+        {
+            { LocationType.Factory, -50 },
+            { LocationType.Hospital, -30 }
+        };
+
+        Event protest = new Event(EventType.SOME_LOCATIONS, "Protest at the School.");
+        protest.locationsEnergyModifier = new Dictionary<LocationType, int>
+        {
+            { LocationType.School, 20 }
+        };
+
+        // Adding events to specific days
+        eventsByDay[1] = new List<Event> { chillDay };
+        eventsByDay[2] = new List<Event> { earthquake };
+        eventsByDay[3] = new List<Event> { protest };
+    }
+
+    private void InitializeEnablingLocationsByDay()
+    {
+        enablingLocationsByDay = new Dictionary<int, LocationType>();
+        enablingLocationsByDay[1] = LocationType.Hospital;
+        enablingLocationsByDay[2] = LocationType.School;
+        enablingLocationsByDay[3] = LocationType.Supermarket;
+        enablingLocationsByDay[4] = LocationType.Cinema;
+        enablingLocationsByDay[5] = LocationType.Club;
     }
 }
