4989f12821341b0a4ff6ac33965256cff509ce86
Nikita <nikita.semonin@gmail.com>
2025-03-29 20:29:44 +0200

    wtf

--- Assets/Global/Scripts/StationManager.cs
+++ Assets/Global/Scripts/StationManager.cs
@@ -1,7 +1,4 @@
-using System;
 using System.Collections.Generic;
-using Scenes.PowerStation.Scripts;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class StationManager  : MonoBehaviour
--- Assets/Scenes/PowerStation/Scripts/CardInfo.cs
+++ Assets/Scenes/PowerStation/Scripts/CardInfo.cs
@@ -1,16 +1,14 @@
-﻿namespace Scenes.PowerStation.Scripts
+﻿
+public class CardInfo
 {
-    public class CardInfo
-    {
-        public int electricity;
-        public string cardName;
-        public string cardDescription;
-        public Event cityEvent;
+    public int electricity;
+    public string cardName;
+    public string cardDescription;
+    public Event cityEvent;
 
+    
+    public CardInfo()
+    {
         
-        public CardInfo()
-        {
-            
-        }
     }
 }
